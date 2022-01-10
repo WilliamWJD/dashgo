0c8643275c4ee69678419ec46426a5bfb712dca6
williamwjd <william.jose.wjd@gmail.com>
2022-01-10 20:17:21 -0300

    responsividade do componente header

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,19 +1,25 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex
             align="center"
         >
-            <Box marginRight="4" textAlign="right">
-                <Text>William dias</Text>
-                <Text
-                    color="gray.300"
-                    fontSize="small"
-                >
-                    william@email.com.br
-                </Text>
-            </Box>
+            {showProfileData && (
+                <Box marginRight="4" textAlign="right">
+                    <Text>William dias</Text>
+                    <Text
+                        color="gray.300"
+                        fontSize="small"
+                    >
+                        william@email.com.br
+                    </Text>
+                </Box>
+            )}
             <Avatar
                 size="md"
                 name="William Dias"
